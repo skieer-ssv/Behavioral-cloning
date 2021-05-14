e73ca9de25125d8cf55fc0401c6ed191d5b23d49
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-05-14 21:33:31 +0530

    Created: drive file which drives the car

---
+++ testSimulation.py
@@ -0,0 +1,71 @@
+print('Setting UP')
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import socketio
+import eventlet
+import numpy as np
+from flask import Flask
+from tensorflow.keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+
+# from utils import *
+#### FOR REAL TIME COMMUNICATION BETWEEN CLIENT AND SERVER
+sio = socketio.Server()
+#### FLASK IS A MICRO WEB FRAMEWORK WRITTEN IN PYTHON
+app = Flask(__name__)  # '__main__'
+
+maxSpeed = 10
+
+def crop(image, top_percent=0.35, bottom_percent=0.1):
+    assert 0 <= top_percent < 0.5, 'top_percent should be between 0.0 and 0.5'
+    assert 0 <= bottom_percent < 0.5, 'top_percent should be between 0.0 and 0.5'
+
+    top = int(np.ceil(image.shape[0] * top_percent))
+    bottom = image.shape[0] - int(np.ceil(image.shape[0] * bottom_percent))
+
+    return image[top:bottom, :]
+
+def preProcess(img):
+    img = crop(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
+    return img
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = preProcess(image)
+    image = np.array([image])
+    steering = float(model.predict(image))
+    throttle = 1.0 - speed / maxSpeed
+    print(f'{steering}, {throttle}, {speed}')
+    sendControl(steering, throttle)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('Connected')
+    sendControl(0, 0)
+
+
+def sendControl(steering, throttle):
+    sio.emit('steer', data={
+        'steering_angle': steering.__str__(),
+        'throttle': throttle.__str__()
+    })
+
+
+if __name__ == '__main__':
+    model = load_model('model.h5')
+    app = socketio.Middleware(sio, app)
+    ### LISTEN TO PORT 4567
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
